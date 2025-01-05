53f2c29c13216609a4e73b673c588732c8b7e0f6
abdulalikhan <abdulalikhan1337@gmail.com>
2025-01-05 23:45:10 +0200

    define token types and basic keyword mapping

---
+++ token/constants.go
@@ -0,0 +1,30 @@
+package token
+
+const (
+	// Identifiers
+	IDENT = "IDENT"
+
+	// Literals
+	INT = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+
+	// Special types
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+)
---
+++ token/token.go
@@ -0,0 +1,22 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type  TokenType
+	Value string
+}
+
+// keywordMap maps literals to language keywords
+var keywordMap = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// GetIdentifierTypeByValue translates identifier values to token types
+func GetIdentifierTypeByValue(identifierVal string) TokenType {
+	if tokenType, ok := keywordMap[identifierVal]; ok {
+		return tokenType
+	}
+	return IDENT
+}
